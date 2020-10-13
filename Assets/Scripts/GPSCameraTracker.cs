6cfce33fa3180860ca6fb4d29645b1023ea181a8
Sander Arjan Benoist <sabenoist5@gmail.com>
2020-10-13 19:25:40 +0200

    fixed gps camera rotation following the user

---
+++ Assets/Scripts/GPSCameraTracker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GPSCameraTracker : MonoBehaviour {
+    public Transform target;
+    public float distance;
+
+    void Update() {
+        transform.position = new Vector3(target.position.x, target.position.y + 20, target.position.z - distance);
+    }
+}
