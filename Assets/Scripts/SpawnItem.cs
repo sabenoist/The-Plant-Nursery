758723e56ae337e1608b684dbe9efb44bab1deba
Sander Arjan Benoist <sabenoist5@gmail.com>
2020-10-14 16:44:29 +0200

    items will now spawn randomly upon startup

--- Assets/Scripts/SpawnItem.cs
+++ Assets/Scripts/SpawnItem.cs
@@ -9,23 +9,29 @@ public class SpawnItem : MonoBehaviour {
     public Vector3 center;
     public Vector3 size;
 
+    public int maxItems;
+    private int itemCounter;
+
     // Start is called before the first frame update
     void Start() {
-        
+        for (int i = 0; i < maxItems; i++) {
+            Spawn();
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey(KeyCode.Q)) {
+        if (itemCounter < maxItems) {
             Spawn();
         }
     }
 
     public void Spawn() {
-        //transform.position = playerTarget.position;
         Vector3 pos = center + new Vector3(Random.Range(playerTarget.position.x - size.x / 2, playerTarget.position.x + size.x / 2), Random.Range(playerTarget.position.y - size.y / 2, playerTarget.position.y + size.y / 2), Random.Range(playerTarget.position.z - size.z / 2, playerTarget.position.x +  size.z / 2));
 
         Instantiate(waterPrefab, pos, Quaternion.identity);
+
+        itemCounter++;
     }
 
     void OnDrawGizmosSelected() {
