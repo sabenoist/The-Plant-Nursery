3bc81323abd0d5298480715f6b00b7e85ae0aee7
JFelixOJ <jfojonsson@protonmail.com>
2020-10-14 21:15:19 +0200

    Felix 5

---
+++ Assets/Scripts/GameObjectHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectHandler : MonoBehaviour
+{
+ 
+    public GameObject myPrefab;
+    private int maxAmount = 0;
+    GameObject Plant_1(Clone);
+    Plant TheScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void createPlant()
+    {
+        if(maxAmount < 3)
+        {
+           Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }       
+    }
+
+    public void watering()
+    {
+        Plant_1(Clone) = GameObject.Find("Plant1");
+        TheScript = Plant_1(Clone).GetComponent<Plant>();
+        TheScript.watering();
+        TheScript.toString();
+    }
+}
