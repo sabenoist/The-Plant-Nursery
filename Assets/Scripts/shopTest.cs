cae3728b08170c0bdb9c2428e835da44a1c73f6e
miyu0201 <y.minghui@yahoo.com>
2020-10-15 22:19:09 +0200

    test

---
+++ Assets/Scripts/shopTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shopTest : MonoBehaviour
+{
+    GameObject Item;
+    GameObject g;
+    [SerializeField] Transform ScrollView;
+
+    void Start ()
+    {
+      Item = ScrollView.GetChild (0).gameObject;
+
+      for  (int i = 0; i<10;i++)
+       {
+         g = Instantiate (Item, ScrollView);
+        }
+
+     Destroy (Item);
+    }
+}
