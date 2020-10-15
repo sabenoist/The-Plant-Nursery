a6978e54a7953a68377c1595d4996eb05dc433f5
miyu0201 <y.minghui@yahoo.com>
2020-10-15 16:01:16 +0200

    background music across scenes

--- Assets/Scripts/ButtonSound.cs
+++ Assets/Scripts/ButtonSound.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ButtonSound : MonoBehaviour
 {
     public AudioSource buttonSound;
+    public AudioSource sellingSound;
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +23,10 @@ public class ButtonSound : MonoBehaviour
     {
        buttonSound.Play();
 
+    }
+
+    public void playSellingSound()
+    {
+      sellingSound.Play();
     }
 }
