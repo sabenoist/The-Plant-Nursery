58b3a3b03b1f4865d2c4056b1156e73028bea2e3
miyu0201 <y.minghui@yahoo.com>
2020-10-16 00:14:05 +0200

    plant AR view test

---
+++ Assets/Scripts/exit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class exitAR : MonoBehaviour
+{
+    public Canvas start;
+    public Button exit;
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
