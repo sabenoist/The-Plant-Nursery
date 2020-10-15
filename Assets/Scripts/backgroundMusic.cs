21730cef33251411d2b84866bb9c2eb4104ea519
miyu0201 <y.minghui@yahoo.com>
2020-10-15 15:32:30 +0200

    button sound effect

---
+++ Assets/Scripts/backgroundMusic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+ public class backgroundMusic : MonoBehaviour
+ {
+ 
+     private static backgroundMusic instance = null;
+     public static backgroundMusic Instance
+     {
+         get { return instance; }
+     }
+     void Awake()
+     {
+         if (instance != null && instance != this) {
+             Destroy(this.gameObject);
+             return;
+         } else {
+             instance = this;
+         }
+         DontDestroyOnLoad(this.gameObject);
+     }
+}
