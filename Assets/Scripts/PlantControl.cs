2ed0f41650a5ce2d19024c00b68c80edc7f159df
miyu0201 <y.minghui@yahoo.com>
2020-10-17 00:40:01 +0200

    choose plant function not finished

---
+++ Assets/Scripts/PlantControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlantControl : MonoBehaviour
+{
+
+    public Text moneyText;
+    public static int moneyAmount;
+    int isItemSold;
+    public GameObject item;
+
+    // Use this for initialization
+  //  
+    void awake()
+    {
+        PlayerPrefs.SetInt("MoneyAmount", 200);
+    }
+      void Start()
+    {
+        
+      moneyAmount = PlayerPrefs.GetInt("MoneyAmount");
+      isItemSold = PlayerPrefs.GetInt("IsItemSold");
+
+      if (isItemSold == 1)
+          item.SetActive(true);
+      else
+          item.SetActive(false);
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        moneyText.text = moneyAmount.ToString();
+
+    }
+
+    public void gotoStore()
+   {
+       PlayerPrefs.SetInt("MoneyAmount", moneyAmount);
+       
+   }
+    public void delete()
+    {
+
+       // buyButton.gameObject.SetActive(true);
+
+        PlayerPrefs.DeleteAll();
+        item.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ShopControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShopControl : MonoBehaviour
+{
+
+    int moneyAmount;
+    int isItemSold;
+
+    public Text moneyAmountText;
+    public Text itemPrice;
+
+    public Button buyButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        moneyAmount = PlayerPrefs.GetInt("MoneyAmount");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moneyAmountText.text = moneyAmount.ToString();
+
+        isItemSold = PlayerPrefs.GetInt("IsItemSold");
+
+        if (moneyAmount >= 50 && isItemSold == 0)
+            buyButton.interactable = true;
+        else
+            buyButton.interactable = false;
+    }
+
+    public void buyItem()
+    {
+        moneyAmount -= 50;
+        PlayerPrefs.SetInt("IsItemSold", 1);
+       // itemPrice.text = "Sold!";
+        buyButton.gameObject.SetActive(false);
+    }
+
+    public void exitShop()
+    {
+        PlayerPrefs.SetInt("MoneyAmount", moneyAmount);
+       
+    }
+
+
+    //public void delete()
+  //  {
+
+    //    buyButton.gameObject.SetActive(true);
+
+      //  PlayerPrefs.DeleteAll();
+   // }
+
+}
