4b0e876f0dd09b54c6d07a5f90b0ddfc7d966fae
Sander Arjan Benoist <sabenoist5@gmail.com>
2020-10-19 23:45:37 +0200

    items should now spawn inside the camera window only for now

---
+++ Assets/Scripts/MainMenu Scripts/ARButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARButtonHandler : MonoBehaviour {
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start() {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (MapData.proximateItem == null) {
+            button.interactable = false;
+        } else {
+            button.interactable = true;
+        }
+    }
+}
