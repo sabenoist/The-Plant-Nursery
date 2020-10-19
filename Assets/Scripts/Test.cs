3c966d6cbea5eca6d0873bdbebe36e7ee03432a5
JFelixOJ <jfojonsson@protonmail.com>
2020-10-19 12:34:18 +0200

    100

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,30 @@
+﻿using IPlantInterface.cs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private GameObjectHandler someScript;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void testFunction()
+    {
+        someScript = GameObject.Find("MotherTree").GetComponent <GameObjectHandler> ();
+        someScript.createPlant(1);
+        someScript.createGameObject();
+        someScript.toString();
+       // someScript.
+       // someScript.createGameObject();
+    }
+
+}
