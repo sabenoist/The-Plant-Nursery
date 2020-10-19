d2d7bfa175851394f6a7415bf1a73eb9a65e7205
miyu0201 <y.minghui@yahoo.com>
2020-10-19 23:14:41 +0200

    testing with GameObject Handler

---
+++ Assets/Scripts/createObject.cs
@@ -0,0 +1,20 @@
+﻿using IPlantInterface.cs;
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor.Rendering;
+using UnityEngine;
+
+public class createObject : MonoBehaviour
+{
+    private GameObjectHandler MotherTree;
+    // Start is called before the first frame update.
+    void Start()
+    {
+        MotherTree = GameObject.Find("MotherTree").GetComponent<GameObjectHandler>();
+    }
+
+    public void createGameObject()
+    {
+        MotherTree.createGameObject();
+    }
+}
