690f75c36bd0ec45fb90492cd928660beb2c2879
miyu0201 <y.minghui@yahoo.com>
2020-10-20 20:08:22 +0200

    edit shop control

--- Assets/Scripts/ShopControl.cs
+++ Assets/Scripts/ShopControl.cs
@@ -29,8 +29,8 @@ public class ShopControl : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-       // MotherTree= GameObject.Find("MotherTree").GetComponent<GameObjectHandler>();
-        if (!PlayerPrefs.HasKey("MoneyAmount")) PlayerPrefs.SetInt("MoneyAmount", 500);
+       
+        if (!PlayerPrefs.HasKey("MoneyAmount")) PlayerPrefs.SetInt("MoneyAmount", 500); //check if no MoneyAmount PlayerPrefs, then initialize PlayerPrefs money amount
         moneyAmount = PlayerPrefs.GetInt("MoneyAmount");
 
     }
@@ -39,7 +39,7 @@ public class ShopControl : MonoBehaviour
     void Update()
     {
 
-        moneyAmountText.text = moneyAmount.ToString();
+        moneyAmountText.text = moneyAmount.ToString(); //mooneyAmount is int
 
         isItemSold1 = PlayerPrefs.GetInt("IsItemSold1");
         isItemSold2 = PlayerPrefs.GetInt("IsItemSold2");
@@ -75,17 +75,17 @@ public class ShopControl : MonoBehaviour
         
     }
 
-    public void buyItem1()
+    public void buyItem1() //refer to plant1 buyButton , if item 1 is sold, set priceText to sold, disable buybutton
     {
         moneyAmount -= 50;
         PlayerPrefs.SetInt("IsItemSold1", 1);
         itemPrice1.text = "Sold!";
         buyButton1.gameObject.SetActive(false);
 
-        LocalPlantHandler = GameObject.Find("test").GetComponent<lokalPlantHandler>();
-        LocalPlantHandler.createPlant(1);
-        LocalPlantHandler.ToString();
-        Debug.LogWarning("Hello, World");
+        LocalPlantHandler = GameObject.Find("test").GetComponent<lokalPlantHandler>(); //get lokalPlantHandler script in test gameobject
+        LocalPlantHandler.createPlant(1); //call createPlant function, sent int 1 to create plantType 1
+        LocalPlantHandler.ToString();//debuging
+        Debug.LogWarning("Hello, World");//debuging
 
 
     }
@@ -113,19 +113,20 @@ public class ShopControl : MonoBehaviour
 
     public void exitShop()
     {
-        PlayerPrefs.SetInt("MoneyAmount", moneyAmount);
+        PlayerPrefs.SetInt("MoneyAmount", moneyAmount);//when exit shop and back to my plant, set playerPrefs
 
     }
 
-
+    /*
     public void resetPlayerPrefs()
     {
-        moneyAmount = 150;
+        //moneyAmount = 150;
        // buyButton1.gameObject.SetActive(true);
        // buyButton2.gameObject.SetActive(true);
        // buyButton3.gameObject.SetActive(true);
         PlayerPrefs.DeleteAll();
     }
+    */
     
 
 }
